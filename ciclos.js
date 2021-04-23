3ec172d694e5c01ca484326a50b69be495487908
ericka0612 <ericka.gm0612@gmail.com>
2021-04-23 13:09:25 -0600

    Tarea de Ciclos en JS

---
+++ ciclos.js
@@ -0,0 +1,27 @@
+//Ejemplos del ciclo While
+
+let i = 0;
+while (i < 3) { 
+alert( i );
+i++;
+}
+
+let i = 3;
+while (i) { 
+alert( i );
+i--;
+}
+
+//Ejemplo ciclo Do While
+
+let i = 0;
+do {
+alert( i );
+i++;
+} while (i < 3);
+
+//Ejemplo del ciclo For
+
+for (let i = 0; i < 3; i++) { // muestra 0, luego 1, luego 2
+    alert(i);
+}
